62e7a1e139ea3380f54556d036b1e87c71a5a057
AstroJr0 <rehanjayed01@gmail.com>
2025-12-17 16:10:26 +0600

    Update __init__.py with new module and version
    Updated version number and added 'solvers' module import.

--- matha/__init__.py
+++ matha/__init__.py
@@ -2,6 +2,7 @@
 # -------------------------
 # @Author : AstroJr0
 # @Date : 16-12-2025
+# #Last-Modified : 17-12-2025
 # -------------------------
 # Import all functions from core modules and expose them
 
@@ -16,14 +17,13 @@ from .regression import *
 from .machine_learning import *
 from .vector import *
 from .special_functions import *
-from .graph import * # Assuming this module exists for plotting
-from .complex import *
+from .graph import *
+from .solvers import *
+
+__version__ = "1.1.0"
 
 # Define __all__ for explicit export control (good practice)
 __all__ = [
     # List functions/classes you guys want to export here, 
     # but using '*' imports above is often simpler for utility libraries. its efficent :)
 ]
-
-# Set a version number for your package
-__version__ = "1.0.0"

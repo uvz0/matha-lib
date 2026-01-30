1192468cb5b1505403ec8f9eebf3676611d559ee
AstroJr0 <rehanjayed01@gmail.com>
2026-01-30 16:46:28 +0600

    Update __init__.py with version and sort import
    Updated version number and added import for sort module.

--- matha/__init__.py
+++ matha/__init__.py
@@ -2,7 +2,7 @@
 # -------------------------
 # @Author : AstroJr0
 # @Date : 16-12-2025
-# #Last-Modified : 17-12-2025
+# @Last-Modified : 30-01-2026
 # -------------------------
 # Import all functions from core modules and expose them
 
@@ -19,12 +19,14 @@ from .vector import *
 from .special_functions import *
 from .graph import *
 from .solvers import *
+from .sort import *
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 
 # Define __all__ for explicit export control (good practice)
 __all__ = [
     # List functions/classes you guys want to export here, 
     # but using '*' imports above is often simpler for utility libraries. its efficent :)
 ]
+
 
